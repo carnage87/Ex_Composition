6067533a31fc1701462478c1753097660adb4e5d
carnage87 <walter.carnage@gmail.com>
2021-09-23 22:25:43 -0300

    Iniciando o código e criada a classe Cliente

---
+++ Ex_Composition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ex_Composition.Entities;
+
+namespace Ex_Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
